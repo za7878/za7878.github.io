b1ce0a79de7d069d5cc09db2ca89069f2f14fd6a
za7878 <1q2w3e927@naver.com>
2021-05-31 20:33:28 +0900

    0531 스터디 과제

---
+++ script.js
@@ -0,0 +1,25 @@
+function clearOutput() {
+    document.getElementById("output").innerHTML="0";
+}
+function removeZero(){
+    var value = document.getElementById("output").innerHTML;
+    if (value=="0"){
+        value = " "
+        document.getElementById("output").innerHTML=value;
+    }
+}
+function perc(){
+    var value = document.getElementById("output").innerHTML;
+    value = value/100
+    document.getElementById("output").innerHTML=value;
+}
+function fordisplay(myvalue){
+    removeZero()
+    document.getElementById("output").innerHTML+=myvalue
+}
+function solve(){
+    removeZero()
+    var equation = document.getElementById("output").innerHTML;
+    var solved = eval(equation);
+    document.getElementById("output").innerHTML=solved;
+}
